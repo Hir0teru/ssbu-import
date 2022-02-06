ee43e149bb8b530e2ea6d8a400353e1b656573b0
hiroteru <78360123+HiraiDaiki@users.noreply.github.com>
2022-02-06 15:45:45 +0900

    jsonファイルを指定のディレクトリに出力可能とする修正

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -10,48 +29,76 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const { GoogleSpreadsheet } = require('google-spreadsheet');
+const fs = __importStar(require("fs"));
 require('dotenv').config();
 const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID);
 // スプレッドシートのヘッダーからkeyを作成する
-const extractKeys = (headers) => {
+const createKeyCollection = (headers, title) => {
     const keys = [];
+    const keyCollection = { [title]: {} };
     headers.forEach((header) => {
-        const key = header.indexOf("−") === -1 ? header : header.substring(0, header.indexOf("−"));
-        if (keys.includes(key)) {
-            return;
-        }
-        else {
+        const key = header.includes("−") ? header.substring(0, header.indexOf("−")) : header;
+        if (!keys.includes(key))
             keys.push(key);
-        }
+        keyCollection[title][key] === undefined ? keyCollection[title][key] = [header] : keyCollection[title][key] = [...keyCollection[title][key], header];
     });
-    return keys;
+    return keyCollection;
 };
+// jsonファイルを出力する
+const outputJSONFile = (path, jsonData) => {
+    fs.writeFile(`${path}/import.json`, jsonData, function (err) {
+        if (err)
+            throw err;
+        console.log('success');
+    });
+};
+const makeDictionary = ((prop, entries) => {
+    const dictionary = {};
+    entries.forEach((entry) => {
+        //const temp: string = entry[prop]
+        //dictionary[temp] = entry;
+    });
+});
 (() => __awaiter(void 0, void 0, void 0, function* () {
     yield doc.useServiceAccountAuth(require('./credentials.json'));
     yield doc.loadInfo();
-    const frameSheet = yield doc.sheetsById["0"];
-    console.log(frameSheet.title);
-    const frameRows = yield frameSheet.getRows();
-    console.log(frameRows);
-    console.log(extractKeys(frameSheet.headerValues));
-    const temp = frameRows.map((row) => {
-        return row['ファイター'];
-    });
-    console.log(temp);
+    const test = ["0", "762307127"];
+    let dictionary = {};
+    for (const t of test) {
+        const frameSheet = yield doc.sheetsById[t];
+        const frameRows = yield frameSheet.getRows();
+        const keyCollection = createKeyCollection(frameSheet.headerValues, frameSheet.title);
+        const title = Object.keys(keyCollection)[0];
+        const keys = Object.keys(keyCollection[title]);
+        let fighters = [];
+        frameRows.forEach((row) => {
+            const fighter = { [title]: {} };
+            keys.forEach((key) => {
+                keyCollection[title][key].forEach((header) => {
+                    if ((key === 'id') || (key === 'name')) {
+                        fighter[key] = row[header];
+                    }
+                    else {
+                        // 横スマッシュ-発生→発生のように文字列を編集してからkeyとする
+                        fighter[title][key] = Object.assign(Object.assign({}, fighter[title][key]), { [header.includes("−") ? header.substring(header.indexOf("−") + 1) : header]: row[header] });
+                    }
+                });
+            });
+        });
+        let dict = {};
+        fighters.forEach((fighter) => {
+            const d = {};
+            if (fighter.id !== undefined) {
+                d[fighter.id] = fighter;
+            }
+            dict = Object.assign(Object.assign({}, dict), d);
+        });
+        const key = Object.keys(dict);
+        key.forEach((k) => {
+            dictionary[k] = Object.assign(Object.assign({}, dictionary[k]), dict[k]);
+        });
+    }
+    outputJSONFile(process.env.JSON_FILE_PAHT, JSON.stringify(dictionary));
 }))().catch(e => {
     console.log(e);
 });
-// シートを受け取ってオブジェクトを返す
-/* const hoge = (sheet: GoogleSpreadsheetWorksheetType): Promise<string> => {
-  const rows: Promise<any> = await sheet.getRows();
-  const keys: string[] = extractKeys(sheet.headerValues);
-  keys.forEach((key: string) => {
-    if ((key === 'No') || (key === 'キャラクター')) {
-      return;
-    }
-  })
-  rows.forEach((row) => {
-    console.log(row);
-  })
-  return new Promise(resolve => resolve('foo'));
-} */
--- index.ts
+++ index.ts
@@ -1,15 +1,32 @@
 const { GoogleSpreadsheet } = require('google-spreadsheet');
+import * as fs from 'fs';
 import type {
   GoogleSpreadsheetWorksheet as GoogleSpreadsheetWorksheetType,
   GoogleSpreadsheet as GoogleSpreadsheetType,
   GoogleSpreadsheetRow
 } from 'google-spreadsheet';
 require('dotenv').config();
+
 type KeyCollection = {
   [title: string]: {
     [key: string]: string[]
   };
 };
+
+type Frames = {
+  [title: string]: {
+    [move: string] : {
+      [data: string]: string
+    }
+  }
+}
+
+type IdAndName = {
+  id?: string
+  name?: string
+}
+
+type Fighter =  IdAndName & Frames;
 
 const doc: GoogleSpreadsheetType = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID);
 
@@ -25,34 +42,62 @@ const createKeyCollection = (headers: string[], title: string): KeyCollection =>
   return keyCollection;
 }
 
+// jsonファイルを出力する
+const outputJSONFile = (path: string | undefined, jsonData: string): void => {
+  fs.writeFile(`${path}/import.json`, jsonData, function (err: any) {
+    if (err) throw err;
+    console.log('success');
+  });
+}
+
 (async (): Promise<void> => {
   await doc.useServiceAccountAuth(require('./credentials.json'));
-
   await doc.loadInfo();
-  const frameSheet: GoogleSpreadsheetWorksheetType = await doc.sheetsById["0"];
-  const frameRows: GoogleSpreadsheetRow[] = await frameSheet.getRows();
-  const temp = frameRows.map((row: {[s: string]: string}) => {
-    return row['ファイター'];
-  })
-  /* console.log(frameSheet.title);
-  console.log(frameRows);
-  console.log(extractKeys(frameSheet.headerValues));
-  console.log(temp) */
+  const test: string[] = ["0", "762307127"];
+  let dictionary: {[id: string] : Fighter} = {};
+  for (const t of test) {
+    const frameSheet: GoogleSpreadsheetWorksheetType = await doc.sheetsById[t];
+    const frameRows: GoogleSpreadsheetRow[] = await frameSheet.getRows();
+    const keyCollection = createKeyCollection(frameSheet.headerValues, frameSheet.title);
+    const title: string = Object.keys(keyCollection)[0];
+    const keys: string[] = Object.keys(keyCollection[title]);
+    let fighters: Fighter[] = [];
+    frameRows.forEach((row: {[key: string]: string}) => {
+      const fighter: Fighter = {[title]: {}};
+      keys.forEach((key: string) => {
+        keyCollection[title][key].forEach((header) => {
+          if ((key === 'id') || (key === 'name')){
+            fighter[key] = row[header]
+          } else {
+            // 横スマッシュ-発生→発生のように文字列を編集してからkeyとする
+            fighter[title][key]= {...fighter[title][key], [header.includes("−") ? header.substring(header.indexOf("−") + 1) : header]: row[header] };
+          }
+        })
+      })
+    })
+    let dict: {[id: string] : Fighter} = {};
+    fighters.forEach((fighter: Fighter) => {
+      const d : {[id: string] : Fighter} = {};
+      if (fighter.id !== undefined) {
+        d[fighter.id] = fighter;
+      }
+      dict = {...dict, ...d};
+    })
+    const key: string[] = Object.keys(dict);
+    key.forEach((k) => {
+      dictionary[k] = {...dictionary[k], ...dict[k]}
+    })
+  }
+  outputJSONFile(process.env.JSON_FILE_PAHT, JSON.stringify(dictionary));
 })().catch(e => {
   console.log(e);
 });
 
-// シートを受け取ってオブジェクトを返す
-/* const hoge = (sheet: GoogleSpreadsheetWorksheetType): Promise<string> => {
-  const rows: Promise<any> = await sheet.getRows();
-  const keys: string[] = extractKeys(sheet.headerValues);
-  keys.forEach((key: string) => {
-    if ((key === 'No') || (key === 'キャラクター')) {
-      return;
-    }
+/* const makeDictionary = ((prop: string, entries: Fighter[]) => {
+  const dictionary: {[key: string]: Fighter} = {};
+  entries.forEach((entry: Fighter) => {
+    //const temp: string = entry[prop]
+    //dictionary[temp] = entry;
   })
-  rows.forEach((row) => {
-    console.log(row);
-  })
-  return new Promise(resolve => resolve('foo'));
-} */
+}); */
+
